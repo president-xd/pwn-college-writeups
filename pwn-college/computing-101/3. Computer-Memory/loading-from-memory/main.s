9b6cfc0e15d757d9de811611f58c2f155c78e618
Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>
2025-02-10 00:57:51 +0500

    Create main.s

---
+++ pwn-college/computing-101/3. Computer-Memory/loading-from-memory/main.s
@@ -0,0 +1,18 @@
+.intel_syntax noprefix
+.global _start
+
+; Load 31337, in RDI Register.
+; You must retrieve this secret number and use it as the exit 
+; code for your program. To do this, you must read it into rdi, 
+; whose value, if you recall, is the first parameter 
+; to exit and is used as the exit code. Good luck!
+
+.section .text
+_start:     
+    mov rdi, [133700]         ; for memory address
+    mov rax, 60
+    syscall
+
+; as -o main.o main.s
+; ld -o main main.o
+; /challenge/check main
