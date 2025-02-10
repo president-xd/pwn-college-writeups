ddebdd64be59af584afa91de4da19698cc17ccb6
Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>
2025-02-10 10:08:18 +0500

    Update main.s

--- pwn-college/3. computing-101/1. your-first-program/1. your-first-register/main.s
+++ pwn-college/3. computing-101/1. your-first-program/1. your-first-register/main.s
@@ -1,12 +1,12 @@
 .intel_syntax noprefix
 .global _start
 
-; Moving 60 in RAX Register
+# Moving 60 in RAX Register
 .section .text
 _start:
     mov rax, 60
 
 
-; as -o main.o main.s
-; ld -o main main.o
-; /challenge/check main
+# as -o main.o main.s
+# ld -o main main.o
+# /challenge/check main
