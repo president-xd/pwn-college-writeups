bdcf25fab2613edf6eb75289b9a48dfff1aaa099
Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>
2025-02-10 10:09:15 +0500

    Update main.s

--- pwn-college/3. computing-101/1. your-first-program/5. moving-between-registers/main.s
+++ pwn-college/3. computing-101/1. your-first-program/5. moving-between-registers/main.s
@@ -1,9 +1,9 @@
 .intel_syntax noprefix
 .global _start
 
-; Anyways, on to the challenge! In this challenge, we will store a 
-; secret value in the rsi register, and your program must exit with that 
-; value as the return code.
+# Anyways, on to the challenge! In this challenge, we will store a 
+# secret value in the rsi register, and your program must exit with that 
+# value as the return code.
 .section .text
 _start:
     mov rdi, rsi
@@ -11,6 +11,6 @@ _start:
     syscall
 
 
-; as -o main.o main.s
-; ld -o main main.o
-; /challenge/check main
+# as -o main.o main.s
+# ld -o main main.o
+# /challenge/check main
