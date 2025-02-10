dd666171c949d0e6faf4156fe0eb160711a7124f
Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>
2025-02-10 10:08:54 +0500

    Update main.s

--- pwn-college/3. computing-101/1. your-first-program/4. building-executables/main.s
+++ pwn-college/3. computing-101/1. your-first-program/4. building-executables/main.s
@@ -1,11 +1,11 @@
 .intel_syntax noprefix
 .global _start
 
-; Using Previous Code, because it will also build an executable
+# Using Previous Code, because it will also build an executable
 
-; Syscall for exiting a program
-; 60 for exit syscall
-; Program Exit Number is 43 which is in RDI
+# Syscall for exiting a program
+# 60 for exit syscall
+# Program Exit Number is 43 which is in RDI
 .section .text
 _start:
     mov edi, 43
@@ -13,6 +13,6 @@ _start:
     syscall
 
 
-; as -o main.o main.s
-; ld -o main main.o
-; /challenge/check main
+# as -o main.o main.s
+# ld -o main main.o
+# /challenge/check main
