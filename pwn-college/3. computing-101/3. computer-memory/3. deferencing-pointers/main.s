e22417998a7a3f7a66fb8c7db19dc080d837ae96
Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>
2025-02-10 10:09:52 +0500

    Update main.s

--- pwn-college/3. computing-101/3. computer-memory/3. deferencing-pointers/main.s
+++ pwn-college/3. computing-101/3. computer-memory/3. deferencing-pointers/main.s
@@ -1,14 +1,14 @@
 .intel_syntax noprefix
 .global _start
 
-; Load memory address of rax in RDI Register.
-; mov rdi, [rax]
+# Load memory address of rax in RDI Register.
+# mov rdi, [rax]
 .section .text
 _start:     
     mov rdi, [rax]         ; for memory address
     mov rax, 60
     syscall
 
-; as -o main.o main.s
-; ld -o main main.o
-; /challenge/check main
+# as -o main.o main.s
+# ld -o main main.o
+# /challenge/check main
